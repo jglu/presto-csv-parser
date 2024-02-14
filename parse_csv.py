1e5a21c5f0cd35be89d1cd7c0c2b949cc6f7b799
jess-jpg <jessicalu2004@gmail.com>
2024-02-13 21:25:31 -0500

    update comment formatting

--- parse_csv.py
+++ parse_csv.py
@@ -3,24 +3,22 @@ import csv
 import os 
 
 os.chdir(os.path.dirname(os.path.abspath(__file__))) # cd to script dir
-filenames = glob("./input/*.csv") # returns a list
-
-sum = 0
+filenames = glob("./input/*.csv") 
 
 with open('results.txt', 'w') as results_file:
+    sum = 0
     for filename in filenames:
         year_sum = 0
         with open(filename, 'r') as csv_file:
             input_reader = csv.reader(csv_file)
-
             next(input_reader) # skip header line
             # loop through all lines
             for line in input_reader:
                 try: 
                     amount = line[4] # get amount only
-
-                    # amount is in pattern "-$x.xx" or "$x.xx"
-                    # if it starts with $, then add it. if $, then minus it.
+                    # amount is in pattern "$x.xx" or "-$x.xx"
+                    #   if it starts with $, then it is given credit.
+                    #   if it starts with -$, then it is cost.
                     if amount.startswith('$'):
                         price = float(amount[1:])
                         sum = round(sum - price, 2) # fix floating point issues
@@ -29,9 +27,9 @@ with open('results.txt', 'w') as results_file:
                         price = float(amount[2:])
                         sum = round(sum + price, 2)
                         year_sum = round(year_sum + price, 2)
-                    print(year_sum)
+                    # print(year_sum)
                 except IndexError:
-                    print("index error. continuing to next file")
+                    # print("index error. continuing to next file")
                     continue
         output_line = "In this year, I've spent $" + str(year_sum) + ".\n"
         results_file.write(output_line)
